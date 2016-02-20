2a1630f10c7a8dad2eb5cec7024fcb28f1ebdf98
Jared King <j@jaredtking.com>
2016-02-20 14:34:16 -0600

    moved instantiation of route objects into separate methods to make it easier to override ModelController

--- src/ModelController.php
+++ src/ModelController.php
@@ -11,6 +11,8 @@ use App\RestApi\Serializer\ChainedSerializer;
 use App\RestApi\Serializer\JsonSerializer;
 use App\RestApi\Serializer\ModelSerializer;
 use Infuse\HasApp;
+use Infuse\Request;
+use Infuse\Response;
 
 class ModelController
 {
@@ -18,45 +20,122 @@ class ModelController
 
     public function create($req, $res)
     {
-        $route = new CreateModelRoute($req, $res);
-        $route->injectApp($this->app)
-              ->setSerializer($this->getSerializer($req))
-              ->run();
+        $this->getCreateRoute($req, $res)->run();
     }
 
     public function findAll($req, $res)
     {
-        $route = new ListModelsRoute($req, $res);
-        $route->injectApp($this->app)
-              ->setSerializer($this->getSerializer($req))
-              ->run();
+        $this->getListRoute($req, $res)->run();
     }
 
     public function retrieve($req, $res)
     {
-        $route = new RetrieveModelRoute($req, $res);
-        $route->injectApp($this->app)
-              ->setSerializer($this->getSerializer($req))
-              ->run();
+        $this->getRetrieveRoute($req, $res)->run();
     }
 
     public function edit($req, $res)
     {
-        $route = new EditModelRoute($req, $res);
-        $route->injectApp($this->app)
-              ->setSerializer($this->getSerializer($req))
-              ->run();
+        $this->getEditRoute($req, $res)->run();
     }
 
     public function delete($req, $res)
     {
-        $route = new DeleteModelRoute($req, $res);
-        $route->injectApp($this->app)
-              ->setSerializer($this->getSerializer($req))
-              ->run();
+        $this->getDeleteRoute($req, $res)->run();
     }
 
-    public function getSerializer($req)
+    /**
+     * Builds a create route object.
+     *
+     * @param Request  $req
+     * @param Response $res
+     *
+     * @return CreateModelRoute
+     */
+    protected function getCreateRoute(Request $req, Response $res)
+    {
+        $route = new CreateModelRoute($req, $res);
+        $route->injectApp($this->app)
+              ->setSerializer($this->getSerializer($req));
+
+        return $route;
+    }
+
+    /**
+     * Builds a list route object.
+     *
+     * @param Request  $req
+     * @param Response $res
+     *
+     * @return ListModelsRoute
+     */
+    protected function getListRoute(Request $req, Response $res)
+    {
+        $route = new ListModelsRoute($req, $res);
+        $route->injectApp($this->app)
+              ->setSerializer($this->getSerializer($req));
+
+        return $route;
+    }
+
+    /**
+     * Builds a retrieve route object.
+     *
+     * @param Request  $req
+     * @param Response $res
+     *
+     * @return RetrieveModelRoute
+     */
+    protected function getRetrieveRoute(Request $req, Response $res)
+    {
+        $route = new RetrieveModelRoute($req, $res);
+        $route->injectApp($this->app)
+              ->setSerializer($this->getSerializer($req));
+
+        return $route;
+    }
+
+    /**
+     * Builds an edit route object.
+     *
+     * @param Request  $req
+     * @param Response $res
+     *
+     * @return EditModelRoute
+     */
+    protected function getEditRoute(Request $req, Response $res)
+    {
+        $route = new EditModelRoute($req, $res);
+        $route->injectApp($this->app)
+              ->setSerializer($this->getSerializer($req));
+
+        return $route;
+    }
+
+    /**
+     * Builds a delete route object.
+     *
+     * @param Request  $req
+     * @param Response $res
+     *
+     * @return DeleteModelRoute
+     */
+    protected function getDeleteRoute(Request $req, Response $res)
+    {
+        $route = new DeleteModelRoute($req, $res);
+        $route->injectApp($this->app)
+              ->setSerializer($this->getSerializer($req));
+
+        return $route;
+    }
+
+    /**
+     * Builds a serializer object.
+     *
+     * @param Request $req
+     *
+     * @return ChainedSerializer
+     */
+    public function getSerializer(Request $req)
     {
         $modelSerializer = new ModelSerializer($req);
         $jsonSerializer = new JsonSerializer($req);
